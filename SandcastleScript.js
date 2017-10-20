8758a8748dd174b47794c1fa5f9cc254a855f201
keithylone <keithylone@gmail.com>
2017-10-19 21:28:53 -0400

    Update SandcastleScript.js

--- SandcastleScript.js
+++ SandcastleScript.js
@@ -2,13 +2,18 @@
 
 Beep = new Audio("https://www.soundjay.com/button/beep-02.mp3");
 	Beep.volume = 1;
+beeped = false;
 
 //Main Program and Loop
 function MainProgram() {
 	document.title = Molpy.Redacted.toggle - Molpy.Redacted.countup; //RK countdown
 	if (Molpy.Redacted.location > 0) {
-		Beep.play(); //beep beep beep
-		while (Molpy.Redacted.location > 0) {}
+		if (beeped == false) {
+			Beep.play(); //beep beep beep
+			beeped = true;
+		}
+	} else {
+		beeped = false;
 	}
 	setTimeout(MainProgram, 900);
 }
