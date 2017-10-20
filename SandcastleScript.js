bf970a6ed4901513865107147eb7ad05ce603be1
keithylone <keithylone@gmail.com>
2017-10-19 21:04:20 -0400

    Update SandcastleScript.js

--- SandcastleScript.js
+++ SandcastleScript.js
@@ -1,28 +1,15 @@
-// Code again from Xenko
+// Code taken from Xenko
 
-var Game = {};
-
-
-Game.Beep = new Audio("https://www.soundjay.com/button/beep-02.mp3");
-	Game.Beep.volume = 1;
-
-
-Game.RedundaKitty = function() {
-	document.title = Molpy.Redacted.toggle - Molpy.Redacted.countup;
-	if (Molpy.Redacted.location > 0) {
-		Game.Beep.play();
-	}
-}
+Beep = new Audio("https://www.soundjay.com/button/beep-02.mp3");
+	Beep.volume = 1;
 
 //Main Program and Loop
 function MainProgram() {
-	Game.RedundaKitty();
-	StartLoop();
-}
-
-StartLoop = function () {
+	document.title = Molpy.Redacted.toggle - Molpy.Redacted.countup; //RK countdown
+	if (Molpy.Redacted.location > 0) {
+		Beep.play(); //beep beep beep
+		while (Molpy.Redacted.location > 0) {}
+	}
 	setTimeout(MainProgram, 900);
 }
-
-console.log("Hi");
 MainProgram();
