fe20307f771536e6b40fb6b6d676f15eff5ce1b1
keithylone <keithylone@gmail.com>
2017-10-19 20:33:28 -0400

    Update SandcastleScriptLoader.js

--- SandcastleScriptLoader.js
+++ SandcastleScriptLoader.js
@@ -1,7 +1,9 @@
 // Code from Xenko
 // http://xenko.github.io/BeachBall/BeachBall.js
 
-function() {
+setTimeout(startup, 1000);
+
+function startup() {
     var js = document.createElement('script');
     js.setAttribute('type', 'text/javascript');
     js.setAttribute('src', 'http://keithylone.github.io/SandcastleScript/SandcastleScript.js');
