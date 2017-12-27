cbbe29c6390ff16477110ff31bae809210787db6
keithylone <keithylone@gmail.com>
2017-12-27 18:18:02 -0500

    Load the actual script rather than itself

--- TrimpScriptLoader.js
+++ TrimpScriptLoader.js
@@ -4,6 +4,6 @@ setTimeout(startup, 1000);
 function startup() {
     var js = document.createElement('script');
     js.setAttribute('type', 'text/javascript');
-    js.setAttribute('src', 'http://keithylone.github.io/TrimpScript/TrimpScriptLoader.js');
+    js.setAttribute('src', 'http://keithylone.github.io/TrimpScript/TrimpScript.js');
     document.head.appendChild(js);
 }
