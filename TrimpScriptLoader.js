5572afa4e06334afbe443c91ccafffe37dabd438
keithylone <keithylone@gmail.com>
2017-12-27 18:04:32 -0500

    Change Sandcastle stuff to Trimps stuff

--- TrimpScriptLoader.js
+++ TrimpScriptLoader.js
@@ -1,11 +1,9 @@
-// Code from Xenko
-// http://xenko.github.io/BeachBall/BeachBall.js
 
 setTimeout(startup, 1000);
 
 function startup() {
     var js = document.createElement('script');
     js.setAttribute('type', 'text/javascript');
-    js.setAttribute('src', 'http://keithylone.github.io/SandcastleScript/SandcastleScript.js');
+    js.setAttribute('src', 'http://keithylone.github.io/TrimpScript/TrimpScriptLoader.js');
     document.head.appendChild(js);
 }
