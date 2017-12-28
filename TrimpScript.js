084711ac97340a9ac208148dcf88af68ac7bb8c5
keithylone <keithylone@gmail.com>
2017-12-27 19:18:04 -0500

    Better Logic

--- TrimpScript.js
+++ TrimpScript.js
@@ -1,19 +1,18 @@
 
-Beep = new Audio("https://www.soundjay.com/button/beep-02.mp3");
+Beep = new Audio("https://www.soundjay.com/misc/censor-beep-01.mp3");
 	Beep.volume = 1;
-beeped = false;
+canBeep = false;
 
 //Main Program and Loop
 function MainProgram() {
 	document.title = balanceStack.textContent; //Balance Stacks
-	if (balanceStack.textContent == "80") {
-		if (beeped == false) {
-			Beep.play(); //beep beep beep
-			beeped = true;
-		}
-	} else {
-		beeped = false;
+	if (balanceStack.textContent == "79") {
+		canBeep = true;
 	}
-	setTimeout(MainProgram, 1000);
+	if (balanceStack.textContent == "80" && canBeep == true) {
+		Beep.play(); //beep beep beep
+		canBeep = false;
+	}
+	setTimeout(MainProgram, 200);
 }
 MainProgram();
