de140a813fbd237e9c398aec72bf8a48f459f57f
keithylone <keithylone@gmail.com>
2018-01-11 21:06:03 -0500

    Update Decay.js
    Better logic

--- Decay.js
+++ Decay.js
@@ -6,8 +6,14 @@ canBeep = false;
 //Main Program and Loop
 function MainProgram() {
 	document.title = decayStackCount.textContent; //Balance Stacks
-	if (decayStackCount.textContent == "150" || decayStackCount.textContent == "170") {
-		Beep.play();
+	if (decayStackCount.textContent == "149" || decayStackCount.textContent =="179") {
+		canBeep = true;
+	}
+	if (decayStackCount.textContent == "150" || decayStackCount.textContent == "180") {
+		if (canBeep) {
+			Beep.play();
+			canBeep = false;
+		}
 	}
 	setTimeout(MainProgram, 200);
 }
